ff267b39d8d160e15a9953d6d5deea3624910bdb
mrpotatoes <andric.droid@gmail.com>
2020-09-18 13:08:38 -0400

    Moving something top-level

--- examples/ts-node/src/index.ts
+++ examples/ts-node/src/index.ts
@@ -1,4 +1,14 @@
-import { meaningOfLife } from "@nighttrax/foo";
+// import { meaningOfLife } from "@nighttrax/foo";
+
+import tpl from '@ts-monorepo-starter/templates'
+
+const vars = {
+  andric: '"Andric" is my name, yo.',
+  names: ['thingy', 'bar', 'baz'],
+  rmWhitespace: true,
+}
+
+const compiled = tpl(vars)
 
 // eslint-disable-next-line no-console
-console.log(meaningOfLife);
+console.log(compiled)
